9a3706322710d82071f3d33e598122869b231998
vagrant <vagrant@localhost.localdomain>
2020-09-24 16:50:37 +0000

    [fix] controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,39 @@
 Rails.application.routes.draw do
 
+
 	scope module: :public do
-	  devise_for :customers, controllers: {
-	  	registrations: 'public/registrations',
-	  	sessions: 'public/sessions'
-	  }
-	  root 'public/items#top'
-	  get 'items/about' => 'public/items"about', as: 'about'
-	end
-
-
-	devise_scope :admin do
-	  	devise_for :admins, path: 'admin', controllers: {
-	  		sessions: 'admins/sessions'
-	  	}
+		devise_for :customers, controllers: {
+			registrations: 'public/registrations',
+			sessions: 'public/sessions'
+		}
+		root 'items#top'
+		resources :addresses, only: [:index, :edit]
+		resources :items, only: [:index, :show]
+			get 'items/top' => 'items#top'
+			get 'items/about' => 'items#about', as: 'about'
+		resources :customers, only: [:show, :edit, :update]
+			get 'customers/unsubscribe' => 'customers#unsubscribe', as: 'unsubscribe'
+			patch 'customers/withdraw' => 'customers#withdraw', as: 'withdraw'
+		resources :cart_items, only: [:index, :create, :update, :destroy] do
+			collection do
+				delete 'destroy_all'
+			end
+		end
+		resources :orders, only: [:new, :index, :show, :create]
+			post 'orders/confirm' => 'orders#confirm', as: 'order_confirm'
+			get 'orders/thanks' => 'orders#thanks', as: 'thanks'
 	end
 
 	namespace :admin do
-		get '/admin' => 'admin#top', as: 'top'
+		devise_for :admin, controllers: {
+			sessions: 'admin/sessions'
+		}
+		resources :customers, only: [:index, :show, :edit, :update]
+		resources :genres, only: [:index, :create, :edit, :update]
+		resources :items, except: [:destroy]
+		get '/' => 'homes#top', as: 'admin_top'
+		resources :orders, only: [:index, :show, :update]
+		resources :order_details, only: [:update]
 	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
