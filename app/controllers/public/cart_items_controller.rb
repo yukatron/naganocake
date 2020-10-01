50d45cc4425c535fa01bebf44900a68084ac9fb3
vagrant <vagrant@localhost.localdomain>
2020-10-01 16:48:33 +0000

    [finish]カート機能

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -2,10 +2,10 @@ class Public::CartItemsController < ApplicationController
 	before_action :authenticate_customer!
 
   def create
-    binding.pry
     if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
       cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
-      cart_item.quantity += params[:quantity]
+      cart_item.quantity += params[:cart_item][:quantity].to_i
+      cart_item.save
       redirect_to items_path
     else
       @cart_item = current_customer.cart_items.build(cart_item_params)
@@ -25,18 +25,36 @@ class Public::CartItemsController < ApplicationController
 
   def destroy
   	cart_item = CartItem.find(params[:id])
-  	cart_item.destroy
+  	if cart_item.destroy
+      flash[:notice] = "カート商品を削除しました"
   	redirect_to cart_items_path
+    else
+      render :index
+    end
+  end
+
+  def update
+    @cart_item = CartItem.find(params[:id])
+    if @cart_item.update(cart_item_params)
+      flash[:notice] = "#{@cart_item.item.name}の数量が変更されました"
+      redirect_to cart_items_path
+    else
+      render :index
+    end
+  end
+
+
+
+  def destroy_all
+    CartItem.where(customer_id: current_customer.id).destroy_all
+    flash[:notice] = "カートを空にしました"
+    redirect_to items_path
   end
 
 
   private
   def cart_item_params
   	params.require(:cart_item).permit(:quantity, :item_id)
-  end
-
-  def set_customer
-    @customer = current_customer
   end
 
 end
--- app/helpers/public/cart_items_helper.rb
+++ app/helpers/public/cart_items_helper.rb
@@ -6,7 +6,13 @@ module Public::CartItemsHelper
 
   	def tax_added_currency(price, rate)
   		taxed_price = (price*rate).round
-  		"#{taxed_price}"
+  		"#{taxed_price.to_s(:delimited, delimiter: ',')}円"
+  	end
+
+  	def subtotal(price, rate, quantity)
+  		taxed_price = (price*rate).round
+  		subtotal = taxed_price*quantity
+  		"#{subtotal.to_s(:delimited, delimiter: ',')}円"
   	end
 
 
