591a246cb9ceb80f651bf555cdaf4acece367995
vagrant <vagrant@localhost.localdomain>
2020-10-25 11:20:48 +0000

    [fix]seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Admin.create( :email => 'admin@test.com', :password => 'password')
+Admin.create(email: 'admin@test.com', password: 'password')
