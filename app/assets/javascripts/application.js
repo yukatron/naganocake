ea03e2fb282b87f7b588232d27ef16f8e21948b8
vagrant <vagrant@localhost.localdomain>
2020-10-25 11:38:21 +0000

    [edit]application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,7 +21,7 @@ $(document).on('turbolinks:load',function () {
       // スライドショーの変化 ("fade" or "slide")
       transition : 'fade',
       // 変化に係る時間(ミリ秒)
-      speed : 1000,
+      speed : 3000,
       // easingの種類
       easing : 'easeOutQuart',
       // ナビゲーションの形("block" or "bubble")
@@ -31,9 +31,9 @@ $(document).on('turbolinks:load',function () {
       // ナビゲーション矢印の表示(trueで表示)
       arrows : true,
       // スライドショーの自動再生(falseで自動再生なし)
-      autoPlay : true,
+      autoPlay : false,
       // 自動再生時のスライド切替間隔(ミリ秒)
-      autoPlayDuration : 1000,
+      autoPlayDuration : 5000,
       // キーボードの矢印キーによるスライド送りの設定(trueで有効)
       keyboardOnAlways : true,
       // 一枚目のスライド表示時に戻る矢印を表示するかどうか(falseで非表示)
